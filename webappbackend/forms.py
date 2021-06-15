1ee69326928db6dc72a05ab63a5ec7a46731dab7
mmdoja <mmdoja@gmail.com>
2021-06-15 17:10:28 +0200

    added validation for username and EmailId

--- webappbackend/forms.py
+++ webappbackend/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+from webappbackend import db_operations
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +14,20 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = db_operations.find_one({
+            "username": username.data
+        })
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = db_operations.find_one({
+            "email": email.data
+        })
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
